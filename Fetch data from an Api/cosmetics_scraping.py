bbd928c0a7363ec1a2299c0d29dbddbd461245c0
hassamrajpoot <64329739+hassamrajpoot@users.noreply.github.com>
2022-06-21 11:43:26 +0500

    Update cosmetics_scraping.py

--- Fetch data from an Api/cosmetics_scraping.py
+++ Fetch data from an Api/cosmetics_scraping.py
@@ -4,7 +4,7 @@ url = 'https://restaurant-api.wolt.com/v4/venues/5f562d24b0a912a096425c5d/menu'
 response = urlopen(url)
 
 json_data = json.loads(response.read())
-with open('/home/hassamrajpoot/Desktop/cosmetics_scraping', 'w') as f:
+with open('', 'w') as f: #File name to write data to
     for item in json_data['items']:
         f.write("\nname: {}".format(item['name']))
         f.write("\ndescription: {}".format(item['description']))
@@ -13,4 +13,4 @@ with open('/home/hassamrajpoot/Desktop/cosmetics_scraping', 'w') as f:
 f.close()
 
 #https://wolt.com/he/isr/tel-aviv/venue/be-pharm-dizengoff?fbclid=IwAR1wlQ2hMqQVxGWeDv-SH5sv-9ofb7Lt9XMqwH8REAe3GmngsTDNbOUGbQU
-#used json response of api request to get information of all the items!
+#used json response of an api request to get information of all the items!
