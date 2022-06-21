ae74d4f6ab5485a27fbf51f22abea67b2a8d8cd9
hassamrajpoot <64329739+hassamrajpoot@users.noreply.github.com>
2022-06-21 13:14:02 +0500

    Update countries_of_the_world_scrapper.py

--- Scrape this site examples/countries_of_the_world_scrapper.py
+++ Scrape this site examples/countries_of_the_world_scrapper.py
@@ -5,7 +5,7 @@ link = "https://scrapethissite.com/pages/simple/"
 raw_data = requests.get(link)
 data = bs(raw_data.content, "html.parser")
 names = data.find_all("div", class_="col-md-4 country")
-with open('', w) as file:
+with open('', w) as file: #File name to write data to
     for name in names:
         country = name.h3.text
         country_info = name.find("div", class_="country-info")
